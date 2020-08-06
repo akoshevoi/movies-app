dbb8e8f6b5e4c5ea8d6c6eaca67f4122c0dd0292
akoshevoi <53705782+akoshevoi@users.noreply.github.com>
2020-08-06 19:32:55 +0300

    commit

--- src/containers/ResultList/ResultList.js
+++ src/containers/ResultList/ResultList.js
@@ -1,7 +1,6 @@
 // @flow
 import React from 'react';
 import { connect } from 'react-redux';
-import Scrollbar from 'react-scrollbars-custom';
 import { bounceInUp } from 'react-animations';
 import Radium, { StyleRoot } from 'radium';
 import Result from '../Result/Result';
@@ -17,20 +16,9 @@ const ResultList = ({ movies }) => (
   <StyleRoot>
     <div className='results'>
       <div className='container  container--content' style={styles.bounceInUp}>
-        <Scrollbar
-          trackYProps={{ className: 'scrollbar-droplist__trackY' }}
-          thumbYProps={{ className: 'scrollbar-droplist__thumbY' }}
-          className='scrollbar-droplist'
-        >
-          {movies.map(item => (
-            <Result key={item.id} item={item} />
-          ))}
-        </Scrollbar>
-        <div className='result__list--adaptive'>
-          {movies.map(item => (
-            <Result key={item.id} item={item} />
-          ))}
-        </div>
+        {movies.map(item => (
+          <Result key={item.id} item={item} />
+        ))}
       </div>
     </div>
   </StyleRoot>
